89d5140f09adb858919c055ee0d840da9b0ff4d7
Ahkar02468 <arkarshwebaw@gmail.com>
2024-04-01 22:36:59 +0700

    event handler as a function

--- part1c/src/App.jsx
+++ part1c/src/App.jsx
@@ -2,11 +2,17 @@ import {useState} from 'react'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
+  const increaseByOne = () => {
+    setCounter(counter + 1)
+  }
+  const setToZero = () => {
+    setCounter(0)
+  }
   return (
     <div>
       {counter}
-      <button onClick={() => setCounter(counter + 1)}>Plus</button>
-      <button onClick={() => setCounter(0)}>Zero</button>
+      <button onClick={increaseByOne}>Plus</button>
+      <button onClick={setToZero}>Zero</button>
     </div>
   )
 }
