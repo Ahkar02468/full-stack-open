5a000e8e01550f69a37a099c5141a45ccb3d3bd3
Ahkar02468 <arkarshwebaw@gmail.com>
2024-04-10 22:29:30 +0700

    handling array with concat to make immutable

--- part1d/src/App.jsx
+++ part1d/src/App.jsx
@@ -1,20 +1,33 @@
 import {useState} from 'react'
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
-  })
+  // const [clicks, setClicks] = useState({
+  //   left: 0,
+  //   right: 0
+  // })
 
-  const handleLeftClicks = () => setClicks({...clicks, left: clicks.left + 1})
+  // const handleLeftClicks = () => setClicks({...clicks, left: clicks.left + 1})
 
-  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
+  // const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClicks = () => {   
+      setAll(allClicks.concat('L'))   
+      setLeft(left + 1)  
+  }
+  const handleRightClick = () => {    
+    setAll(allClicks.concat('R'))    
+    setRight(right + 1) 
+  }
   return (
     <div>
-      {clicks.left} 
+      {left} 
       <button onClick={handleLeftClicks}>Left</button>
       <button onClick={handleRightClick}>Right</button>
-      {clicks.right}
+      {right}
+      {allClicks.join(" ")}
     </div>
   )
 }
