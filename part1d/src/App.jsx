5b0a6e48030e61fdd223a659d4269d5f85b129df
Ahkar02468 <arkarshwebaw@gmail.com>
2024-04-19 23:20:02 +0700

    test conditional rendering

--- part1d/src/App.jsx
+++ part1d/src/App.jsx
@@ -10,24 +10,51 @@ const App = () => {
 
   // const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
 
+  const History = (props) => {
+    if(props.allClicks.length === 0){
+      return(
+        <div>
+          the app is used by pressing the button
+        </div>
+      )
+    }
+    return(
+      <div>
+        button press history: {props.allClicks.join(" ")}
+      </div>
+    )
+  }
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
-  const handleLeftClicks = () => {   
+  const [total, setTotal] = useState(0)
+  const handleLeftClick = () => {   
       setAll(allClicks.concat('L'))   
-      setLeft(left + 1)  
+      console.log("leftbefore", left)
+      const updatedLeft = left + 1
+      setLeft(updatedLeft)
+      console.log("leftAfter", left)  
+      setTotal(updatedLeft + right)
   }
   const handleRightClick = () => {    
     setAll(allClicks.concat('R'))    
     setRight(right + 1) 
+    setTotal(left + right)
   }
   return (
     <div>
       {left} 
-      <button onClick={handleLeftClicks}>Left</button>
-      <button onClick={handleRightClick}>Right</button>
+      <Button handleClick={handleLeftClick} text='left' />      
+      <Button handleClick={handleRightClick} text='right' />
       {right}
-      {allClicks.join(" ")}
+      <History allClicks={allClicks}/>
     </div>
   )
 }
