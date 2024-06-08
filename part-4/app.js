f1f17952147e9c4888a2cb26967385f2d980e16d
Ahkar02468 <arkarshwebaw@gmail.com>
2024-06-08 15:22:09 +0700

    done ex 4.15

--- part-4/app.js
+++ part-4/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 
 mongoose.set('strictQuery', false)
@@ -20,6 +21,7 @@ app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users',usersRouter)
 
 
 module.exports = app
